a017255dd7bfde8bf364113ecc93976d22adeb32
Robbana Fahd <robbana.fahd99@gmail.com>
2022-10-02 23:38:05 +0100

    Atelier 3

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -15,11 +15,15 @@ class ClubController extends AbstractController
             'controller_name' => 'ClubController',
         ]);
     }
+
+
+
     #[Route('/formations', name: 'list_formation')]
     public function formations()
     {
         $v1= "3A31";
         $v2="J13";
+
         $formations = array(
             array('ref' => 'form147', 'Titre' => 'Formation Symfony
 4','Description'=>'pratique',
@@ -31,15 +35,17 @@ class ClubController extends AbstractController
             array('ref'=>'form178','Titre'=>'Formation Angular' ,
                 'Description'=>'theorique','date_debut'=>'10/06/2020','date_fin'=>'14/06/2020',
                 'nb_participants'=>12));
+
+
         return $this->
         render("club/formations.html.twig",
         array("v1"=>$v1,"v2"=>$v2,"listFormation"=>$formations));
     }
 
-    #[Route('/reservation', name: 'app_reservation')]
-    public function reservationFormation()
+    #[Route('/reservation/{titre}', name: 'app_reservation')]
+    public function reservationFormation($titre)
     {
-        //return new Response("réservation!");
-        return $this->render("club/detail.html.twig");
+
+        return $this->render("club/detail.html.twig",array("titre"=>$titre));
     }
 }
